45c60fd5ffae23cd5363ab6706b0efc344b5a9ed
Donald Stufft <donald@stufft.io>
2014-11-01 01:03:33 -0400

    Remove ip_addrs, should use network.ip_addrs instead

--- salt/_modules/ip_picker.py
+++ salt/_modules/ip_picker.py
@@ -15,29 +15,6 @@ def __virtual__():
     return True
 
 
-def ip_addrs(interface=None, include_loopback=False, cidr=None):
-    '''
-    Returns a list of IPv4 addresses assigned to the host. 127.0.0.1 is
-    ignored, unless 'include_loopback=True' is indicated. If 'interface' is
-    provided, then only IP addresses from that interface will be returned.
-    Providing a CIDR via 'cidr="10.0.0.0/8"' will return only the addresses
-    which are within that subnet.
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        salt '*' network.ip_addrs
-    '''
-    addrs = salt.utils.network.ip_addrs(interface=interface,
-                                        include_loopback=include_loopback)
-    if cidr:
-        return sorted([i for i in addrs if in_subnet(cidr, [i])],
-                      key=lambda item: socket.inet_aton(item))
-    else:
-        return addrs
-
-
 def interfaces_for_cidr(cidr='0.0.0.0/0'):
     '''
     Return a dictionary of information about all the interfaces on the minion
