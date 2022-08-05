a05506138314bed698fcde9ee93342dee5965e0b
Ee Durbin <ewdurbin@gmail.com>
2022-08-05 07:43:02 -0400

    Add a requirements.txt and switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-try:
-    import sphinx_rtd_theme
-except ImportError:
-    sphinx_rtd_theme = None
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -99,11 +94,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-if sphinx_rtd_theme:
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_theme = "default"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
