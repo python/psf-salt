522060a58495d7ae0967c16a10869dcf11ef07fd
Jacob Coffee <jacob@z7x.org>
2024-07-10 13:14:57 -0500

    docs: add copybutton to code blocks

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ from datetime import datetime
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser', "sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
